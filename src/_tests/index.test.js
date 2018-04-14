3630fb99717dc8f75e37bbda9215aba8b1816079
Matt Perry <mattgperry@gmail.com>
2018-04-14 20:13:51 +0100

    latest

--- src/_tests/index.test.js
+++ src/_tests/index.test.js
@@ -1,7 +1,21 @@
-import { vec2 } from '../';
+import { vec2, vec4 } from '../';
 
 test('vec2()', () => {
-  const testVector = vec2(10, 20);
+  const v = vec2(10, 20);
 
-  expect(testVector).toEqual([10, 20]);
+  // Creation
+  expect(vec2(10)).toEqual([10, 10]);
+  expect(v).toEqual([10, 20]);
+  expect(vec4(1, 2, 3, 4)).toEqual([1, 2, 3, 4]);
+  expect(vec4([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  expect(vec4(vec2([1, 2]), vec2([3, 4]))).toEqual([1, 2, 3, 4]);
+
+  // Get
+  expect(v.length).toEqual(2);
+  expect(v.x).toEqual(10);
+  expect(v.yx).toEqual([20, 10]);
+  expect(v.xxxx).toEqual([10, 10, 10, 10]);
+
+  v.y = 30;
+  expect(v.y).toEqual(30);
 });
--- src/index.js
+++ src/index.js
@@ -1,85 +1,113 @@
-const indicies = {
-  x: 0,
-  y: 1,
-  z: 2,
-  r: 0,
-  g: 1,
-  b: 2,
-  a: 3,
-  h: 0,
-  s: 1,
-  l: 2
+// Fill a given array to the size provided with
+// a repeating pattern of its existing values. ie ([10, 20], 3) -> [10, 20, 10]
+const fillVectorArray = (values, size) => {
+  const newValues = flatten(values);
+  const numValues = newValues.length;
+
+  // If we already have the correct number of values, return early
+  if (numValues === size) return newValues;
+
+  // Set array to correct size
+  newValues.length = size;
+
+  // Fill in the blanks
+  for (let i = 0; i < size; i++) {
+    // We're using modulo to wrap the index to within the values range
+    // so vec4(10, 20) becomes [10, 20, 10, 20]
+    if (newValues[i] === undefined)
+      newValues[i] = values[(i % numValues + numValues) % numValues];
+  }
+
+  return newValues;
 };
 
-const vector = numDimensions => {
-  const vectorProxy = {};
+// Flatten an array of values
+const flattenItem = (acc, val) => {
+  if (Array.isArray(val)) {
+    const numItems = val.length;
+    for (let i = 0; i < numItems; i++) acc.push(val[i]);
+  } else {
+    acc.push(val);
+  }
 
-  return (...args) => {
-    const props = typeof args[0] !== 'number' ? args[0] : args;
-    return new Proxy([...props], vectorProxy);
+  return acc;
+};
+
+const flatten = arr => arr.reduce(flattenItem, []);
+
+const makeIndicesMap = (map, key, i) => ((map[key] = i), map);
+
+const getSwizzleKeys = (key, indices) => {
+  if (
+    // If this isn't a string, bail
+    typeof key !== 'string' ||
+    // Check if this is an Array method
+    Array.prototype.hasOwnProperty(key) ||
+    // Index-based access keys are sent as strings, so here we parse
+    // and test to see if they're actually numbers. If so, bail.
+    !isNaN(parseInt(key))
+  )
+    return false;
+
+  // Iterate through all the keys and test to see if they exist in
+  // the indices map. If one doesn't, bail.
+  const keys = key.split('');
+  const numKeys = keys.length;
+  for (let i = 0; i < numKeys; i++) {
+    if (indices[keys[i]] === undefined) return false;
+  }
+
+  return keys;
+};
+
+const getVectorFactory = (factories, size) => factories[size - 2];
+
+const getSwizzled = (target, indices, keys, vec) => {
+  const numKeys = keys.length;
+  return numKeys === 1
+    ? target[indices[keys[0]]]
+    : keys.reduce((acc, key, i) => {
+        acc[i] = target[indices[key]];
+        return acc;
+      }, getVectorFactory(vec, numKeys)());
+};
+
+const setSwizzled = (target, indices, keys, value) => {
+  const numKeys = keys.length;
+  for (let i = 0; i < numKeys; i++) target[indices[keys[i]]];
+  return value;
+};
+
+const vector = axisOrder => {
+  const indices = axisOrder.reduce(makeIndicesMap, {});
+  const vecFactories = [];
+
+  const vectorProxy = {
+    get(target, key, receiver) {
+      const swizzleKeys = getSwizzleKeys(key, indices);
+      return swizzleKeys
+        ? getSwizzled(target, indices, swizzleKeys, vecFactories)
+        : Reflect.get(target, key, receiver);
+    },
+    set(target, key, value, receiver) {
+      const swizzleKeys = getSwizzleKeys(key, indices);
+      return swizzleKeys
+        ? setSwizzled(target, indices, swizzleKeys, value)
+        : Reflect.set(target, key, value, receiver);
+    }
   };
+
+  return axisOrder.reduce((acc, _, i) => {
+    if (i > 0) {
+      acc.push(
+        (...values) => new Proxy(fillVectorArray(values, i + 1), vectorProxy)
+      );
+    }
+    return acc;
+  }, vecFactories);
 };
 
-export const vec2 = vector(2);
-export const vec3 = vector(3);
-export const vec4 = vector(4);
-
-// const getVectorPropByKey = (target, key) => {
-//   const numDimensions = key.length;
-
-//   // If a single dimension like x or r, return plain number
-//   if (numDimensions === 1) {
-//     const index = indicies[key];
-//     return target[index];
-
-//     // Or if multi-dimension, return a vector
-//   } else {
-//     const dimensions = key.split('');
-//     return dimensions.reduce((thisVector, axisKey, i) => {
-//       const index = indicies[axisKey];
-//       thisVector[i] = target[index];
-//       return thisVector;
-//     }, vec([]));
-//   }
-// }
-
-// const getVectorProp = (target, key) => (!isNaN(parseInt(key)))
-//   ? target[parseInt(key)]
-//   : getVectorPropByKey(target, key)
-
-// const setVectorPropByKey = (target, key, value) => {
-//   const i = indicies[key];
-//   target[i] = value;
-//   return target;
-// }
-
-// const vectorProxy = {
-//   get(target, key, receiver) {
-//     return (typeof key === 'string' && key !== 'length')
-//       ? getVectorProp(target, key)
-//       : Reflect.get(target, key, receiver);
-//   },
-
-//   set(target, key, value, receiver) {
-//     const index = parseInt(key);
-//     return (!isNaN(index))
-//       ? Reflect.set(target, key, value, receiver)
-//       : setVectorPropByKey(target, key, value)
-//   },
-//   ownKeys() {
-//     return ['length']
-//   }
-// }
-
-// const vec = (...args) => {
-//   const props = typeof args[0] !== 'number' ? args[0] : args;
-//   return new Proxy([...props], vectorProxy);
-// }
-
-// const xy = vec(0, 35)
-
-// console.log('get', xy.rgb)
-// xy.xy = 20
-// console.log('set', xy)
-
-// //.vec2(xy)
+export { vector };
+export const [vec2, vec3, vec4] = vector(['x', 'y', 'z', 'w']);
+export const [rg, rgb, rgba] = vector(['r', 'g', 'b', 'a']);
+export const [hs, hsl, hsla] = vector(['h', 's', 'l', 'a']);
